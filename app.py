353be3feb68ac98972afaa9c77a1c3e5762385a9
nelsonalanis4-glitch <nelsonalanis4@gmail.com>
2026-04-10 11:58:47 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,61 @@ cursor = conn.cursor()
 
 @app.route("/")
 def home():
-	return "Sistema online funcionando"
+    return """
+    <html>
+    <head>
+        <title>Tienda Online</title>
+        <style>
+            body {
+                background: #1e1e1e;
+                color: white;
+                font-family: Arial;
+                text-align: center;
+            }
+            input, button {
+                padding: 10px;
+                margin: 5px;
+                border-radius: 5px;
+                border: none;
+            }
+            button {
+                background: #4CAF50;
+                color: white;
+            }
+        </style>
+    </head>
+    <body>
+
+        <h2>??? Sistema de Tienda</h2>
+
+        <input id="codigo" placeholder="Código"><br>
+        <input id="cantidad" placeholder="Cantidad"><br>
+
+        <button onclick="vender()">Vender</button>
+
+        <h3>Resultado:</h3>
+        <pre id="res"></pre>
+
+        <script>
+        function vender(){
+            fetch('/vender',{
+                method:'POST',
+                headers:{'Content-Type':'application/json'},
+                body: JSON.stringify({
+                    codigo: document.getElementById('codigo').value,
+                    cantidad: parseInt(document.getElementById('cantidad').value)
+                })
+            })
+            .then(r=>r.json())
+            .then(d=>{
+                document.getElementById('res').innerText = JSON.stringify(d,null,2)
+            })
+        }
+        </script>
+
+    </body>
+    </html>
+    """
 
 @app.route("/productos")
 def productos():
